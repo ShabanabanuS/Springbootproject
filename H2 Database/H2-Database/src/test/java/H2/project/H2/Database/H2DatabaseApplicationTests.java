14b4f053dd54e762ed1a6b5a43a740aa94784d47
Shabana Banu.S <111066770+ShabanabanuS@users.noreply.github.com>
2024-06-24 22:20:42 +0530

    Add files via upload

---
+++ H2 Database/H2-Database/src/test/java/H2/project/H2/Database/H2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package H2.project.H2.Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
